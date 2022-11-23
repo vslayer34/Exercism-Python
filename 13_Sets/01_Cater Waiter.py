c86460705a829eac081727369c2a024da4277cd9
vslayer34 <mohamd.elnagar.34@gmail.com>
2022-11-23 15:42:44 +0200

    Update Cater Waiter

---
+++ 13_Sets/01_Cater Waiter.py
@@ -0,0 +1,228 @@
+from helper import display_task_name, display_example
+
+"""
+Functions for compiling dishes and ingredients for a catering company.
+https://exercism.org/tracks/python/exercises/cater-waiter
+"""
+
+
+from sets_categories_data import (VEGAN,
+                                  VEGETARIAN,
+                                  KETO,
+                                  PALEO,
+                                  OMNIVORE,
+                                  ALCOHOLS,
+                                  SPECIAL_INGREDIENTS)
+
+
+def clean_ingredients(dish_name: str, dish_ingredients: list):
+    """Remove duplicates from `dish_ingredients`.
+
+    :param dish_name: str - containing the dish name.
+    :param dish_ingredients: list - dish ingredients.
+    :return: tuple - containing (dish_name, ingredient set).
+
+    This function should return a `tuple` with the name of the dish as the first item,
+    followed by the de-duped `set` of ingredients as the second item.
+    """
+    dish_filtered = set(dish_ingredients)
+
+    dish_tuple = (dish_name, dish_filtered)
+    return dish_tuple
+
+
+def check_drinks(drink_name: str, drink_ingredients: list):
+    """Append "Cocktail" (alcohol)  or "Mocktail" (no alcohol) to `drink_name`, based on `drink_ingredients`.
+
+    :param drink_name: str - name of the drink.
+    :param drink_ingredients: list - ingredients in the drink.
+    :return: str - drink_name appended with "Mocktail" or "Cocktail".
+
+    The function should return the name of the drink followed by "Mocktail" (non-alcoholic) and drink
+    name followed by "Cocktail" (includes alcohol).
+
+    """
+    drink_ingredients_set = set(drink_ingredients)
+    if drink_ingredients_set.isdisjoint(ALCOHOLS):
+        return f"{drink_name} Mocktail"
+    else:
+        return f"{drink_name} Cocktail"
+
+
+def categorize_dish(dish_name: str, dish_ingredients: list):
+    """Categorize `dish_name` based on `dish_ingredients`.
+
+    :param dish_name: str - dish to be categorized.
+    :param dish_ingredients: list - ingredients for the dish.
+    :return: str - the dish name appended with ": <CATEGORY>".
+
+    This function should return a string with the `dish name: <CATEGORY>` (which meal category the dish belongs to).
+    `<CATEGORY>` can be any one of  (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
+    All dishes will "fit" into one of the categories imported from `sets_categories_data.py`
+
+    """
+    dish_ingredients_set = set(dish_ingredients)
+    list_of_categories_names = [
+        "VEGAN",
+        "VEGETARIAN", 
+        "PALEO",
+        "KETO",
+        "OMNIVORE"
+    ]
+
+    list_of_categories_sets = [
+        VEGAN,
+        VEGETARIAN, 
+        PALEO,
+        KETO,
+        OMNIVORE
+    ]
+    for index, indiviual in enumerate(list_of_categories_sets):
+
+        if dish_ingredients_set.issubset(indiviual):
+            return f"{dish_name}: {list_of_categories_names[index]}"
+
+
+def tag_special_ingredients(dish: tuple):
+    """Compare `dish` ingredients to `SPECIAL_INGREDIENTS`.
+
+    :param dish: tuple - of (dish name, list of dish ingredients).
+    :return: tuple - containing (dish name, dish special ingredients).
+
+    Return the dish name followed by the `set` of ingredients that require a special note on the dish description.
+    For the purposes of this exercise, all allergens or special ingredients that need to be tracked are in the
+    SPECIAL_INGREDIENTS constant imported from `sets_categories_data.py`.
+    """
+    set_of_ingredients = set(dish[1])
+    set_of_special_ingredients = set_of_ingredients.intersection(SPECIAL_INGREDIENTS)
+    return (dish[0], set_of_special_ingredients)
+
+
+def compile_ingredients(dishes: list):
+    """Create a master list of ingredients.
+
+    :param dishes: list - of dish ingredient sets.
+    :return: set - of ingredients compiled from `dishes`.
+
+    This function should return a `set` of all ingredients from all listed dishes.
+    """
+    temp = set(dishes[0])
+    for index in range(0, len(dishes)):
+        set_of_master_ingredients = temp.union(dishes[index])
+    
+    return set_of_master_ingredients
+
+
+def separate_appetizers(dishes, appetizers):
+    """Determine which `dishes` are designated `appetizers` and remove them.
+
+    :param dishes: list - of dish names.
+    :param appetizers: list - of appetizer names.
+    :return: list - of dish names that do not appear on appetizer list.
+
+    The function should return the list of dish names with appetizer names removed.
+    Either list could contain duplicates and may require de-duping.
+    """
+
+    pass
+
+
+def singleton_ingredients(dishes, intersection):
+    """Determine which `dishes` have a singleton ingredient (an ingredient that only appears once across dishes).
+
+    :param dishes: list - of ingredient sets.
+    :param intersection: constant - can be one of `<CATEGORY>_INTERSECTIONS` constants imported from `sets_categories_data.py`.
+    :return: set - containing singleton ingredients.
+
+    Each dish is represented by a `set` of its ingredients.
+
+    Each `<CATEGORY>_INTERSECTIONS` is an `intersection` of all dishes in the category. `<CATEGORY>` can be any one of:
+        (VEGAN, VEGETARIAN, PALEO, KETO, or OMNIVORE).
+
+    The function should return a `set` of ingredients that only appear in a single dish.
+    """
+
+    pass
+
+
+
+# Task 1
+display_task_name("I", "Clean up Dish Ingredients")
+display_example(
+    "clean_ingredients('Punjabi-Style Chole', ['onions', 'tomatoes', 'ginger paste', 'garlic paste', 'ginger paste', 'vegetable oil', 'bay leaves', 'cloves', 'cardamom', 'cilantro', 'peppercorns', 'cumin powder', 'chickpeas', 'coriander powder', 'red chili powder', 'ground turmeric', 'garam masala', 'chickpeas', 'ginger', 'cilantro'])", "('Punjabi-Style Chole', {'garam masala', 'bay leaves', 'ground turmeric', 'ginger', 'garlic paste', 'peppercorns', 'ginger paste', 'red chili powder', 'cardamom', 'chickpeas', 'cumin powder', 'vegetable oil', 'tomatoes', 'coriander powder', 'onions', 'cilantro', 'cloves'})"
+)
+
+print(clean_ingredients('Punjabi-Style Chole', ['onions', 'tomatoes', 'ginger paste', 'garlic paste', 'ginger paste', 'vegetable oil', 'bay leaves', 'cloves', 'cardamom', 'cilantro', 'peppercorns', 'cumin powder', 'chickpeas', 'coriander powder', 'red chili powder', 'ground turmeric', 'garam masala', 'chickpeas', 'ginger', 'cilantro']), "\n")
+
+
+
+
+# Task 2
+display_task_name("II", "Cocktails and Mocktails")
+display_example(
+    "check_drinks('Honeydew Cucumber', ['honeydew', 'coconut water', 'mint leaves', 'lime juice', 'salt', 'english cucumber'])", 
+    'Honeydew Cucumber Mocktail'
+)
+print(check_drinks('Honeydew Cucumber', ['honeydew', 'coconut water', 'mint leaves', 'lime juice', 'salt', 'english cucumber']), "\n")
+
+display_example(
+    "check_drinks('Shirley Tonic', ['cinnamon stick', 'scotch', 'whole cloves', 'ginger', 'pomegranate juice', 'sugar', 'club soda'])", 
+    "Shirley Tonic Cocktail"
+)
+print(check_drinks('Shirley Tonic', ['cinnamon stick', 'scotch', 'whole cloves', 'ginger', 'pomegranate juice', 'sugar', 'club soda']), "\n")
+
+
+
+# Task 3
+display_task_name("III", "Categorize Dishes")
+display_example(
+    "categorize_dish('Sticky Lemon Tofu', {'tofu', 'soy sauce', 'salt', 'black pepper', 'cornstarch', 'vegetable oil', 'garlic', 'ginger', 'water', 'vegetable stock', 'lemon juice', 'lemon zest', 'sugar'})", 
+    "Sticky Lemon Tofu: VEGAN"
+)
+
+print(categorize_dish('Sticky Lemon Tofu', {'tofu', 'soy sauce', 'salt', 'black pepper', 'cornstarch', 'vegetable oil', 'garlic', 'ginger', 'water', 'vegetable stock', 'lemon juice', 'lemon zest', 'sugar'}), "\n")
+
+display_example(
+    "categorize_dish('Shrimp Bacon and Crispy Chickpea Tacos with Salsa de Guacamole', {'shrimp', 'bacon', 'avocado', 'chickpeas', 'fresh tortillas', 'sea salt', 'guajillo chile', 'slivered almonds', 'olive oil', 'butter', 'black pepper', 'garlic', 'onion'})", 
+    "Shrimp Bacon and Crispy Chickpea Tacos with Salsa de Guacamole: OMNIVORE"
+)
+
+print(categorize_dish('Shrimp Bacon and Crispy Chickpea Tacos with Salsa de Guacamole', {'shrimp', 'bacon', 'avocado', 'chickpeas', 'fresh tortillas', 'sea salt', 'guajillo chile', 'slivered almonds', 'olive oil', 'butter', 'black pepper', 'garlic', 'onion'}), "\n")
+
+
+
+
+# Task 4
+display_task_name("IV", "Label Allergens and Restricted Foods")
+display_example(
+    "tag_special_ingredients(('Ginger Glazed Tofu Cutlets', ['tofu', 'soy sauce', 'ginger', 'corn starch', 'garlic', 'brown sugar', 'sesame seeds', 'lemon juice']))", 
+    "('Ginger Glazed Tofu Cutlets', {'garlic','soy sauce','tofu'})"
+)
+print(tag_special_ingredients(('Ginger Glazed Tofu Cutlets', ['tofu', 'soy sauce', 'ginger', 'corn starch', 'garlic', 'brown sugar', 'sesame seeds', 'lemon juice'])), "\n")
+
+
+display_example(
+    "tag_special_ingredients(('Arugula and Roasted Pork Salad', ['pork tenderloin', 'arugula', 'pears', 'blue cheese', 'pine nuts', 'balsamic vinegar', 'onions', 'black pepper']))", 
+    "('Arugula and Roasted Pork Salad', {'pork tenderloin', 'blue cheese', 'pine nuts', 'onions'})"
+)
+
+print(tag_special_ingredients(('Arugula and Roasted Pork Salad', ['pork tenderloin', 'arugula', 'pears', 'blue cheese', 'pine nuts', 'balsamic vinegar', 'onions', 'black pepper'])), "\n")
+
+
+
+
+
+# Task 5
+dishes = [ {'tofu', 'soy sauce', 'ginger', 'corn starch', 'garlic', 'brown sugar', 'sesame seeds', 'lemon juice'},
+           {'pork tenderloin', 'arugula', 'pears', 'blue cheese', 'pine nuts',
+           'balsamic vinegar', 'onions', 'black pepper'},
+           {'honeydew', 'coconut water', 'mint leaves', 'lime juice', 'salt', 'english cucumber'}]
+
+display_task_name('V', 'Compile a "Master List" of Ingredients')
+
+display_example(
+    "compile_ingredients(dishes)", 
+    "{'arugula', 'brown sugar', 'honeydew', 'coconut water', 'english cucumber', 'balsamic vinegar', 'mint leaves', 'pears', 'pork tenderloin', 'ginger', 'blue cheese', 'soy sauce', 'sesame seeds', 'black pepper', 'garlic', 'lime juice', 'corn starch', 'pine nuts', 'lemon juice', 'onions', 'salt', 'tofu'}"
+)
+
+print(compile_ingredients(dishes), "\n")
