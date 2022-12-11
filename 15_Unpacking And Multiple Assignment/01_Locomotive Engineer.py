ddecf839af306c0c78c4379d458ada83bc0e1598
vslayer34 <mohamd.elnagar.34@gmail.com>
2022-12-11 07:27:02 +0200

    Solved first two tasks in Locomotive Engineer

--- 15_Unpacking And Multiple Assignment/01_Locomotive Engineer.py
+++ 15_Unpacking And Multiple Assignment/01_Locomotive Engineer.py
@@ -7,24 +7,33 @@ https://exercism.org/tracks/python/exercises/locomotive-engineer
 
 
 # TODO: define the 'get_list_of_wagons' function
-def get_list_of_wagons():
+def get_list_of_wagons(*args) -> list:
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    pass
+    # *list_of_args = *args
+
+    return list(args)
 
 
 # TODO: define the 'fixListOfWagons()' function
-def fix_list_of_wagons(each_wagons_id, missing_wagons):
+def fix_list_of_wagons(each_wagons_id: list, missing_wagons: list) -> list:
     """Fix the list of wagons.
 
     :parm each_wagons_id: list - the list of wagons.
     :parm missing_wagons: list - the list of missing wagons.
     :return: list - list of wagons.
     """
-    pass
+
+    correct_list = []
+
+    first_index, second_index, id_one, *rest = each_wagons_id
+
+    *correct_list, = id_one, *missing_wagons, *rest, first_index, second_index
+
+    return correct_list
 
 
 # TODO: define the 'add_missing_stops()' function
@@ -57,3 +66,23 @@ def fix_wagon_depot(wagons_rows):
     :return: list[tuple] - list of rows of wagons.
     """
     pass
+
+
+
+# Task 1
+display_task_name("I", "Create a list of all wagons")
+display_example(
+    "get_list_of_wagons(1, 7, 12, 3, 14, 8, 5)",
+    "[1, 7, 12, 3, 14, 8, 3]"
+)
+print(get_list_of_wagons(1, 7, 12, 3, 14, 8, 5), "\n")
+
+
+
+# Task 2 
+display_task_name("II", "Fix the list of wagons")
+display_example(
+    "fix_list_of_wagons([2, 5, 1, 7, 4, 12, 6, 3, 13], [3, 17, 6, 15])",
+    "[1, 3, 17, 6, 15, 7, 4, 12, 6, 3, 13, 2, 5]"
+)
+print(fix_list_of_wagons([2, 5, 1, 7, 4, 12, 6, 3, 13], [3, 17, 6, 15]), "\n")
