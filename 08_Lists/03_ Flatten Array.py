dd66c590b6e5eb13ef4cf1bb2b6b85dcfa46f538
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-02-21 17:20:38 +0200

    tried my hand at  Flatten Array

---
+++ 08_Lists/03_ Flatten Array.py
@@ -0,0 +1,56 @@
+from helper import display_task_name, display_example
+
+'''
+Exersice:  Flatten Array
+URL: https://exercism.org/tracks/python/exercises/flatten-array
+
+
+
+Instructions
+
+Take a nested list and return a single flattened list with all values except nil/null.
+
+The challenge is to write a function that accepts an arbitrarily-deep nested list-like structure and returns a flattened structure without any nil/null values.
+
+For example:
+
+input: [1,[2,3,null,4],[null],5]
+
+output: [1,2,3,4,5]
+'''
+
+
+def flatten(iterable: list) -> list:
+    
+    return [ num for num in iterable ]
+
+
+
+
+# Test 1
+display_task_name("I", "FlattenArray > empty")
+display_example(
+    'flatten([])', '[]'
+)
+print(flatten([]), "\n")
+
+
+
+# Test 2
+display_task_name("II", "FlattenArray > no nesting")
+display_example(
+    'flatten([0, 1, 2])',
+    '[0, 1, 2]'
+)
+
+print(flatten([0, 1, 2]), "\n")
+
+
+
+# Test 3
+display_task_name("III", "FlattenArray > flattens a nested array")
+display_example(
+    'flatten([[[]]])',
+    '[]'
+)
+print(flatten([[[]]]), "\n")
