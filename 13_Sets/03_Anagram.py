a1f1f2df5e20ce8494418bad59bdaedf96869bc2
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-03-14 12:55:37 +0200

    Solved Anagram

--- 13_Sets/03_Anagram.py
+++ 13_Sets/03_Anagram.py
@@ -19,19 +19,25 @@ Given the target "stone" and candidates "stone", "tones", "banana", "tons", "not
 def find_anagrams(word: str, candidates: list) -> list:
     empty_list = []
     
-    # create a set of the world
-    word_set = {letter.lower() for letter in word}
+    # # create a set of the world
+    # word_set = {letter.lower() for letter in word}
 
-    # create a list consistent of sets of the words in it
-    list_of_candidates_letter = [{*word.lower()} for word in candidates]
+    # # create a list consistent of sets of the words in it
+    # list_of_candidates_letter = [{*word.lower()} for word in candidates]
     
-    list_of_equal_sets = [ word_set == set for set in list_of_candidates_letter]
+    # list_of_equal_sets = [ word_set == set for set in list_of_candidates_letter]
 
-    for i in range(0, len(list_of_equal_sets)):
-        if list_of_equal_sets[i] == True:
-            empty_list.append(candidates[i])
+    # for i in range(0, len(list_of_equal_sets)):
+    #     if list_of_equal_sets[i] == True:
+    #         empty_list.append(candidates[i])
     
+    # return empty_list
+    for candidate in candidates:
+        if candidate.casefold() != word.casefold() and sorted(candidate.casefold()) == sorted(word.casefold()):
+            empty_list.append(candidate)
+        
     return empty_list
+
 
 
 
@@ -63,3 +69,12 @@ display_example(
     '[]'
 )
 print(find_anagrams("good", ["dog", "goody"]), "\n")
+
+
+# Test2
+display_task_name("II", "Anagram > detects two anagrams")
+display_example(
+    'find_anagrams("solemn", ["lemons", "cherry", "melons"])',
+    ["lemons", "melons"]
+)
+print(find_anagrams("solemn", ["lemons", "cherry", "melons"]), "\n")
